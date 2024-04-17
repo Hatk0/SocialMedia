07a0b2ec39c67b0b489cf760147daa4b3ebe789c
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-04-17 20:41:24 +0300

    Initial Commit

---
+++ SocialMedia/SocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaApp.swift
+//  SocialMedia
+//
+//  Created by Dmitry Yastrebov on 17.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
