6e34d10af203152b81edfc9d9b4953e4c0b38072
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-04-18 16:13:48 +0300

    Create TextFieldViewModifier

---
+++ SocialMedia/Utilities/ViewModifiers/TextFieldViewModifier.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct TextFieldViewModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.subheadline)
+            .padding(12)
+            .background(Color(.systemGray6))
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .padding(.horizontal, 24)
+    }
+}
