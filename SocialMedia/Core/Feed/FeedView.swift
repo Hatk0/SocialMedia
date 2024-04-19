e206e67ab39fb76da067effa284ac2eca60b5793
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-04-19 15:17:33 +0300

    FeedView Startup Preparation

--- SocialMedia/Core/Feed/FeedView.swift
+++ SocialMedia/Core/Feed/FeedView.swift
@@ -2,7 +2,16 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                LazyVStack {
+                    ForEach(0...10, id: \.self) { thread in
+                        Text("Some text")
+                    }
+                }
+            }
+            .scrollIndicators(.hidden)
+        }
     }
 }
 
