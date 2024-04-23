08cd83d050401979d792b1e1194c46d83cac81a8
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-04-23 13:16:44 +0300

    Create CreationView

--- SocialMedia/Core/Creation/CreationView.swift
+++ SocialMedia/Core/Creation/CreationView.swift
@@ -1,8 +1,67 @@
 import SwiftUI
 
 struct CreationView: View {
+    @State private var startThread = ""
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                HStack(alignment: .top) {
+                    CircularProfileImageView()
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("albertdera")
+                            .fontWeight(.semibold)
+                        
+                        TextField("Start a thread", text: $startThread, axis: .vertical)
+                    }
+                    .font(.footnote)
+                    
+                    Spacer()
+                    
+                    if !startThread.isEmpty {
+                        Button {
+                            startThread = ""
+                        } label: {
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 12, height: 12)
+                                .foregroundStyle(.gray)
+                        }
+                    }
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("New Thread")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("Cancel")
+                    }
+                    .font(.subheadline)
+                    .foregroundStyle(.black)
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Text("Post")
+                    }
+                    .opacity(startThread.isEmpty ? 0.5 : 1)
+                    .disabled(startThread.isEmpty)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.black)
+                }
+            }
+        }
     }
 }
 
